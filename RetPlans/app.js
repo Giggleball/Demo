4864126bbd66db058c3f40399e4b18d82836ad87
Giggleball <boldewijn.eli@gmail.com>
2016-10-21 17:49:05 +0200

    part 1 complete, trying to figure out part 2

--- RetPlans/app.js
+++ RetPlans/app.js
@@ -1,34 +1,109 @@
-'use strict'
 // including the necessary modules
 const express = require( 'express' )
 const fs = require( 'fs' )
 const app = express()
+
 // in order to use bodyParser, you need to install and require it first
 const bodyParser = require('body-parser')
+
+//static will become default and overwrite /home
+app.use( express.static( 'static') )
 
 // which visual template you'll be using 
 app.set( 'view engine', 'pug' )
 app.set( 'views', __dirname + '/views' )
 
-// Need this to use the middleware .body
-app.use( bodyParser.urlencoded({ extended: true }))
+// // Need this to use the middleware .body
+// app.use( bodyParser.urlencoded({ extended: true }))
 
 // when home is requested, show the following
 app.get( '/', ( request, response ) => {
-	console.log( 'About to render index.pug' )
+	console.log( 'Going to homepage' )		
+		response.render( 'index' )
+	})
+
+
+// will be redirected to this page after adding a user
+app.post('/request', ( request, response ) => {
+	let name = request.body.addString
+	console.log( "info requested" ) 
+
+		// Create a javascript object with the array in it
+		let newUser = { 
+			Firstname: request.body.fname, 
+			Lastname: request.body.lname, 
+			CurrentAge:	request.body.cage
+			StartingCapital: request.body.startc		
+			MonthlyContribution: request.body.madd	
+			AgeOfRetirement: request.body.rage	
+			Email: request.body.email
+			InterestRate: request.body.inrateb
+			InterestRate: request.body.inraten
+			InterestRate: request.body.inrateg
+		}
+
+		// a var where we want to store the new data in
+		let newFile = JSON.parse( data )
+
+		// Add some data to 
+		newFile.push( newUser )
+
+		// use fs to write the file to disk
+		fs.writeFile( 'users.json', JSON.stringify( newFile ), 'utf8', ( err, result ) => {
+			console.log('file written')
+		} ); 
+	response.render( 'request' )
+})
+
+
+
+
+
+// will be redirected to this page when searching for a user
+app.post('/users', ( request, response ) => {
+	let name = request.body.searchString
+	console.log( name ) // equal to the name you want to search
+	//find out if searchname is equal to a name in users.json
+
+	//fs readfile
 	fs.readFile( __dirname + '/users.json', ( error, data ) => {
 		if ( error ) throw error
-			let parsedData = JSON.parse( data )
-		console.log( parsedData )
-		response.render( 'index', { data: parsedData } )
+
+		// within readfile: parse the data
+	let dataObject = JSON.parse( data )
+	let searchResults = []
+		// loop through parseddata & check if name == 1st name || last name
+		for ( i = 0; i < dataObject.length; i++) {
+			if ( dataObject[i].firstname == name || dataObject[i].lastname == name ){
+				console.log( "Firstname: " + dataObject[i].firstname )
+				console.log( "Lastname: " + dataObject[i].lastname )
+				// you want to iterate and push your data into the empty array
+				searchResults.push(dataObject[i])
+			}
+		}
+		// the data is stored in the empty array, so you put that as the value for the key
+		response.render( 'users', { results: searchResults } )
+		
+		// if thats the case, we want to render a new page, showing all the data of the user we found
 	})
 })
 
 
 
 
-// Import necessary modules
-//const fs = require( 'fs' )
+
+
+
+
+
+
+
+
+
+// Calculations
+
+Import necessary modules
+const fs = require( 'fs' )
 
 // Helper functions to Round off # & make it more readible
 let roundD = ( number ) => {
@@ -93,6 +168,7 @@ module.exports = calcCompound
 
 
 
+// Loading the server
 
 app.listen(8000, () => {
 	console.log( 'Server running' )
@@ -103,7 +179,7 @@ app.listen(8000, () => {
 
 
 
-// Phase #1
+// Phase #1 √
 // Create a web form where people can input their retirement plans
 // Requirements:
 // * The page uses a framework like bootstrap/materialize
